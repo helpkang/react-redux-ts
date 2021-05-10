05c17fdeed4acd5b7208d05663045fabb53263ea
kang_sungil <helpkang@gmail.com>
2021-05-10 15:32:16 +0900

    모듈 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import 'App.css';
 import { Todo } from 'views/todo/Todo';
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NotFound from 'NotFound';
 function App() {
   return (
+    <Router>
     <div>
-      <Todo></Todo>
+      <Switch>
+        <Route exact path={'/'} component={Todo}/>
+        <Route component={NotFound} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
